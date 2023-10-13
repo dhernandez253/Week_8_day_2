db58849c1d40a87b8bf8577a9e9bb6bd6fbf4d75
dhernandez253 <142819789+dhernandez253@users.noreply.github.com>
2023-10-13 14:38:08 +0000

    i worked on comparison operators

---
+++ logicalOperators.py
@@ -0,0 +1,32 @@
+def logicalOperators():
+   
+  #####################logical operators#############################################
+  
+  # Logical Operators Practice #1
+  # Create three variables (num1, num2, and num3):
+  num1 = 36
+  num2 = 72/2
+  num3 = 48
+  # Inside num1, store the value 36
+  
+  # Inside num2, stores the result of the operation 72/2
+  
+  # Inside num3, store the value 48
+  
+  # Check if num1 is greater than num2, and less than num3. Store the result of that comparison in a variable called my_bool.
+  my_bool = num1 > num2 < num3
+  print(my_bool)
+    
+  #   Logical Operators Practice #2
+  # Check if the words:
+  
+  word1 = "success"
+  
+  word2 = "technology"
+  
+  # are not found in the sentence below, and store the result (a boolean) in a variable called my_bool:
+  my_bool = "success" not in "technology"
+  print(my_bool)
+  # "When something is important enough, you do it even if the odds are against you" - Elon Musk
+  ##################### end logical operators#############################################
+  
